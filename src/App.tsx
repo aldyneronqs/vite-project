d36b7ebee7e88a55a60e916bc835072136c5906d
aldyneronqs <aldyneronquillo91403@gmail.com>
2025-02-06 13:15:29 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import './App.css';
+
+interface ProfileProps {
+  text: string,
+  border: string,
+  color: string,
+  height: string,
+  width: string
+}
+
+function Profile({ text, border, color, height, width } : ProfileProps) {
+  return (
+    <div
+      style={{
+        border: `3px solid ${border}`, 
+        backgroundColor: color,
+        height: `${height}px`,
+        width: `${width}px`,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: 'black', 
+        fontWeight: 'bold', 
+      }}
+    >
+      {text}
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <section style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <h1>ALDYNE RONQUILLO</h1>
+      <Profile 
+        text="CPEITEL" 
+        border="black" 
+        color="pink" 
+        height={'200'} 
+        width={'200'} 
+      />
+    </section>
+  );
+}
+
+export default App;
