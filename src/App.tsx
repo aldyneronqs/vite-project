57b85fb9e91cd04b6685f3093e4203bfab6d4bd0
aldyneronqs <aldyneronquillo91403@gmail.com>
2025-02-06 14:17:59 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -35,9 +35,6 @@ function App() {
       flexDirection: 'column', 
       alignItems: 'center', 
       justifyContent: 'center', 
-      backgroundColor: 'white',
-      width: 100vh,
-      height: 100vh
     }}>
       <h1 style={{ color: 'black', textAlign: 'center' }}>ALDYNE RONQUILLO</h1>
       <Profile 
